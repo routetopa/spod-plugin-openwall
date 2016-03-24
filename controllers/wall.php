c3c734bbe8984c79819a1740d7a989c78a1cef18
andrpet <andrpet@gmail.com>
2016-03-24 11:31:49 +0100

    openwall

--- controllers/wall.php
+++ controllers/wall.php
@@ -96,6 +96,9 @@ class OPENWALL_CTRL_Wall extends OW_ActionController
     {
         OW::getLanguage()->addKeyForJs('openwall', 'admin_title');
 
+
+        OW::getDocument()->getMasterPage()->setTemplate(OW::getThemeManager()->getMasterPageTemplate('general_no_header_pic'));
+
         $this->setPageTitle(OW::getLanguage()->text('openwall', 'index_page_title'));
         $this->setPageHeading(OW::getLanguage()->text('openwall', 'index_page_heading'));
         $this->setDocumentKey('openwall_index_page');
